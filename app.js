c1597f4952b3a6853a4774994c57a600d29f046c
amarjagrup <46466064+amarjagrup@users.noreply.github.com>
2020-10-11 18:05:36 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,109 @@
+//lines 2 to 10 allow the use of packages. 
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/url";
+const multer  = require('multer');
+const shortid = require("shortid");
+const ejs = require('ejs');
+
+//set the template engine to ejs
+app.set('view engine', 'ejs');
+
+app.use(express.static( path.join(__dirname, "/public")));
+
+
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+  
+  console.log("Connected successfully to server");
+ 
+  const db = client.db('url');
+});
+
+//multer needs somewhere to store the images
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, 'public/uploads/'));
+  },
+  // this part keeps the orginal filename
+  filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname));
+
+  }
+});
+
+//upload a single image
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 10000000},
+  fileFilter: function(req, file, cb){
+    const filetypes = /jpeg|jpg|png|gif|svg|heic|heif|webp|pdf/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+  
+    if(mimetype && extname){
+      return cb(null,true);
+    } else {
+      cb('Error: wrong file !');
+    }
+  }
+}).single('img');
+
+
+//loads index.ejs
+app.get('/', (req, res) => {
+
+  res.render('index.ejs');
+});
+
+//when upload button is hit the tinyurl and image is displayed. 
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      res.render('index', {
+        msg: err
+      });
+    } else {
+      if(req.file == undefined){
+        res.render('index', {
+          msg: 'Error: No File Selected!'
+        });
+      } else {
+        const userId = shortid.generate(req.file.path); 
+
+        const img= {
+          contentType: req.file.mimetype,
+          originalname: req.file.originalname,
+          path: req.file.path,
+          tinyUrl:userId
+        };
+        console.log(img)
+        //add data to database
+        MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+          const db = client.db('url');
+           db.collection("TinyUrl").insertOne(img, function(err, res) {
+          if (err) throw err;
+          client.close();
+           })
+        })
+        
+        const str = JSON.stringify(img)
+        const val = JSON.parse(str)
+        console.log(req.file.filename)
+        res.render('index', {
+          msg: 'File Uploaded!',
+          file:`uploads/${req.file.filename}`,
+          msg2: "orginal url is ",
+          val2: `${val['path']}`,
+          val: `${val['tinyUrl']}`,
+          msg3: 'Tiny url is '
+        });
+      }
+    }
+  });
+});
+app.listen(port,() => console.log(`Listening on port ${port}...`))
